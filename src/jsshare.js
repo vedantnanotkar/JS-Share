839b4f214ff07055d938edd3c2878331be721e30
Vedant Nanotkar <78738393+vedantnanotkar@users.noreply.github.com>
2023-01-01 18:30:49 +0530

    Update jsshare.js

--- src/jsshare.js
+++ src/jsshare.js
@@ -39,6 +39,13 @@
 
  */
 
+var shareItems = document.querySelectorAll('.social_share');
+ for (var i = 0; i < shareItems.length; i += 1) {
+   shareItems[i].addEventListener('click', function share(e) {
+     return JSShare.go(this);
+   });
+ }
+
 
 ;(function (factory) {
   var registeredInModuleLoader;
